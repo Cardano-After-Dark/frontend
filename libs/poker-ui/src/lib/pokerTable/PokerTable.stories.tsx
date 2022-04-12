c02533d3d38a0e30f0d7aceb64371ecdcf83c04c
David Baxter <baxter30@gmail.com>
2022-04-11 19:39:12 -0700

    updated stories

--- libs/poker-ui/src/lib/pokerTable/PokerTable.stories.tsx
+++ libs/poker-ui/src/lib/pokerTable/PokerTable.stories.tsx
@@ -1,4 +1,7 @@
+import styled from '@emotion/styled';
+import { Stack } from '@mui/material';
 import { Story, Meta } from '@storybook/react';
+import PokerCard, { CardValues, Suits } from '../pokerCard/PokerCard';
 import { PokerTable } from './PokerTable';
 
 export default {
@@ -6,7 +9,117 @@ export default {
   title: 'PokerTable',
 } as Meta;
 
-const Template: Story = (args) => <PokerTable {...args} />;
+const Template: Story = (args) => (
+  <PokerTable
+    hand={args['hand']}
+    players={args['players']}
+    river={args['river']}
+    {...args}
+  />
+);
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  hand: {
+    name: 'david',
+    bank: 320,
+    bet: 2300,
+    cards: [
+      <PokerCard
+        suit={Suits.Clubs}
+        value={CardValues.Ace}
+        scaleSize={0.4}
+        key={18}
+      />,
+      <PokerCard
+        suit={Suits.Hearts}
+        value={CardValues.King}
+        scaleSize={0.4}
+        key={188}
+        tilt
+      />,
+    ],
+  },
+  players: [
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 1300,
+      cards: [
+        <PokerCard scaleSize={0.3} key={1} />,
+        <PokerCard scaleSize={0.3} key={11} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 1300,
+      cards: [
+        <PokerCard scaleSize={0.3} key={12} />,
+        <PokerCard scaleSize={0.3} key={122} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 1300,
+      cards: [
+        <PokerCard scaleSize={0.3} key={13} />,
+        <PokerCard scaleSize={0.3} key={133} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 1300,
+      cards: [
+        <PokerCard scaleSize={0.3} key={14} />,
+        <PokerCard scaleSize={0.3} key={144} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 200,
+      cards: [
+        <PokerCard scaleSize={0.3} key={15} />,
+        <PokerCard scaleSize={0.3} key={155} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 200,
+      cards: [
+        <PokerCard scaleSize={0.3} key={16} />,
+        <PokerCard scaleSize={0.3} key={166} tilt />,
+      ],
+    },
+    {
+      name: 'Henry',
+      bank: 890,
+      bet: 200,
+      cards: [
+        <PokerCard scaleSize={0.3} key={17} />,
+        <PokerCard scaleSize={0.3} key={177} tilt />,
+      ],
+    },
+  ],
+  river: (
+    <Stack direction={'row'} spacing={1}>
+      <PokerCard suit={Suits.Clubs} value={CardValues.Ace} scaleSize={0.5} />
+      <PokerCard suit={Suits.Hearts} value={CardValues.King} scaleSize={0.5} />
+      <PokerCard
+        suit={Suits.Diamonds}
+        value={CardValues.Eight}
+        scaleSize={0.5}
+      />
+      <PokerCard suit={Suits.Spades} value={CardValues.Seven} scaleSize={0.5} />
+      <PokerCard
+        suit={Suits.Diamonds}
+        value={CardValues.Eight}
+        scaleSize={0.5}
+      />
+    </Stack>
+  ),
+};
--- libs/poker-ui/src/lib/pokerTable/bet/Bet.stories.tsx
+++ libs/poker-ui/src/lib/pokerTable/bet/Bet.stories.tsx
@@ -1,12 +1,36 @@
+import styled from '@emotion/styled';
+import { Box, Typography } from '@mui/material';
 import { Story, Meta } from '@storybook/react';
-import { Bet } from './Bet';
+import { Bet, BetPosition } from './Bet';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #4aad4a;
+`;
 
 export default {
   component: Bet,
   title: 'Bet',
 } as Meta;
 
-const Template: Story = (args) => <Bet {...args} />;
+const Template: Story = (args) => (
+  <Wrapper>
+    <Box
+      sx={{
+        position: 'absolute',
+        bottom: '50%',
+        left: '50%',
+      }}
+    >
+      <Typography variant="h5">Player here</Typography>
+      <Bet amount={args['amount']} position={args['position']} {...args} />
+    </Box>
+  </Wrapper>
+);
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  amount: 123,
+  position: BetPosition.Bottom,
+};
