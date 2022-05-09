7c42b3a19eb5605369cc93c2a24206d4a75cbc82
David Baxter <david.baxter@carvana.com>
2022-05-09 11:23:02 -0700

    updated stories for table

--- libs/poker-ui/src/lib/pokerTable/PokerTable.stories.tsx
+++ libs/poker-ui/src/lib/pokerTable/PokerTable.stories.tsx
@@ -1,124 +1,140 @@
 import { Stack } from '@mui/material';
 import { Story, Meta } from '@storybook/react';
 import PokerCard, { CardValues, Suits } from '../pokerCard/PokerCard';
+import Hand from './hand/Hand';
+import Players from './playerHand/Players';
 import { PokerTable } from './PokerTable';
+import River from './river/River';
 
 export default {
   component: PokerTable,
   title: 'PokerTable',
 } as Meta;
 
-const Template: Story = (args) => (
-  <PokerTable
-    hand={args['hand']}
-    players={args['players']}
-    river={args['river']}
-    {...args}
-  />
-);
+const Template: Story = (args) => <PokerTable>{args['children']}</PokerTable>;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  hand: {
-    name: 'david',
-    bank: 320,
-    bet: 2300,
-    cards: [
-      <PokerCard
-        suit={Suits.Clubs}
-        value={CardValues.Ace}
-        scaleSize={0.4}
-        key={18}
-      />,
-      <PokerCard
-        suit={Suits.Hearts}
-        value={CardValues.King}
-        scaleSize={0.4}
-        key={188}
-        tilt
-      />,
-    ],
-  },
-  players: [
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 1300,
-      cards: [
-        <PokerCard scaleSize={0.3} key={1} />,
-        <PokerCard scaleSize={0.3} key={11} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 1300,
-      cards: [
-        <PokerCard scaleSize={0.3} key={12} />,
-        <PokerCard scaleSize={0.3} key={122} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 1300,
-      cards: [
-        <PokerCard scaleSize={0.3} key={13} />,
-        <PokerCard scaleSize={0.3} key={133} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 1300,
-      cards: [
-        <PokerCard scaleSize={0.3} key={14} />,
-        <PokerCard scaleSize={0.3} key={144} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 200,
-      cards: [
-        <PokerCard scaleSize={0.3} key={15} />,
-        <PokerCard scaleSize={0.3} key={155} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 200,
-      cards: [
-        <PokerCard scaleSize={0.3} key={16} />,
-        <PokerCard scaleSize={0.3} key={166} tilt />,
-      ],
-    },
-    {
-      name: 'Henry',
-      bank: 890,
-      bet: 200,
-      cards: [
-        <PokerCard scaleSize={0.3} key={17} />,
-        <PokerCard scaleSize={0.3} key={177} tilt />,
-      ],
-    },
-  ],
-  river: (
-    <Stack direction={'row'} spacing={1}>
-      <PokerCard suit={Suits.Clubs} value={CardValues.Ace} scaleSize={0.5} />
-      <PokerCard suit={Suits.Hearts} value={CardValues.King} scaleSize={0.5} />
-      <PokerCard
-        suit={Suits.Diamonds}
-        value={CardValues.Eight}
-        scaleSize={0.5}
+  children: (
+    <>
+      <River>
+        <Stack direction={'row'} spacing={1}>
+          <PokerCard
+            suit={Suits.Clubs}
+            value={CardValues.Ace}
+            scaleSize={0.7}
+          />
+          <PokerCard
+            suit={Suits.Hearts}
+            value={CardValues.King}
+            scaleSize={0.7}
+          />
+          <PokerCard
+            suit={Suits.Diamonds}
+            value={CardValues.Eight}
+            scaleSize={0.7}
+          />
+          <PokerCard
+            suit={Suits.Spades}
+            value={CardValues.Seven}
+            scaleSize={0.7}
+          />
+          <PokerCard
+            suit={Suits.Diamonds}
+            value={CardValues.Eight}
+            scaleSize={0.7}
+          />
+        </Stack>
+      </River>
+      <Hand
+        player={{
+          name: 'david',
+          bank: 320,
+          bet: 2300,
+          cards: [
+            <PokerCard
+              suit={Suits.Clubs}
+              value={CardValues.Ace}
+              scaleSize={0.6}
+              key={18}
+            />,
+            <PokerCard
+              suit={Suits.Hearts}
+              value={CardValues.King}
+              scaleSize={0.6}
+              key={188}
+              tilt
+            />,
+          ],
+        }}
       />
-      <PokerCard suit={Suits.Spades} value={CardValues.Seven} scaleSize={0.5} />
-      <PokerCard
-        suit={Suits.Diamonds}
-        value={CardValues.Eight}
-        scaleSize={0.5}
+      <Players
+        players={[
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 1300,
+            cards: [
+              <PokerCard scaleSize={0.5} key={1} />,
+              <PokerCard scaleSize={0.5} key={11} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 1300,
+            cards: [
+              <PokerCard scaleSize={0.5} key={12} />,
+              <PokerCard scaleSize={0.5} key={122} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 1300,
+            cards: [
+              <PokerCard scaleSize={0.5} key={13} />,
+              <PokerCard scaleSize={0.5} key={133} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 1300,
+            cards: [
+              <PokerCard scaleSize={0.5} key={14} />,
+              <PokerCard scaleSize={0.5} key={144} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 200,
+            cards: [
+              <PokerCard scaleSize={0.5} key={15} />,
+              <PokerCard scaleSize={0.5} key={155} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 200,
+            cards: [
+              <PokerCard scaleSize={0.5} key={16} />,
+              <PokerCard scaleSize={0.5} key={166} tilt />,
+            ],
+          },
+          {
+            name: 'Henry',
+            bank: 890,
+            bet: 200,
+            cards: [
+              <PokerCard scaleSize={0.5} key={17} />,
+              <PokerCard scaleSize={0.5} key={177} tilt />,
+            ],
+          },
+        ]}
       />
-    </Stack>
+    </>
   ),
 };
